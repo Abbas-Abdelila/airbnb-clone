698e1245475a2f28ec3b875bb97e67a91c13e896
Abbas Abdelila <abbasabdelila@gmail.com>
2021-08-05 20:27:57 +0300

    airbnb-clone homepage

---
+++ components/Footer.js
@@ -0,0 +1,52 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600 cursor-pointer">
+      <div className="space-y-4 text-xs text-gray-800 ">
+        <h5 className="font-bold">ABOUT</h5>
+        <p className="hover:underline">How Airbnb works</p>
+        <p className="hover:underline">Investors</p>
+        <p className="hover:underline">Newsroom</p>
+        <p className="hover:underline">Airbnb Plus</p>
+        <p className="hover:underline">Airbnb Luxe</p>
+        <p className="hover:underline">HotelTonight</p>
+        <p className="hover:underline">Airbnb for work</p>
+        <p className="hover:underline">Made possible by Hosts</p>
+        <p className="hover:underline">Careers</p>
+        <p className="hover:underline">Founders' Letter</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p className="hover:underline">Diversity & Beloging</p>
+        <p className="hover:underline">Against Discrimination</p>
+        <p className="hover:underline">Accessibility</p>
+        <p className="hover:underline">Airbnb Associates</p>
+        <p className="hover:underline">Frontline Stays</p>
+        <p className="hover:underline">Guest Referals</p>
+        <p className="hover:underline">Gift Cards</p>
+        <p className="hover:underline">Airbnb.org</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p className="hover:underline">Host your home</p>
+        <p className="hover:underline">Host an Online Experience</p>
+        <p className="hover:underline">Host an Experience</p>
+        <p className="hover:underline">Responsible Hosting</p>
+        <p className="hover:underline">Resource Center</p>
+        <p className="hover:underline">Community Center</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p className="hover:underline">Our Covid-19 Responses</p>
+        <p className="hover:underline">Help Centers</p>
+        <p className="hover:underline">Cancellation Options</p>
+        <p className="hover:underline">Neighboorhood Support</p>
+        <p className="hover:underline">Trust & Safety</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ components/MediumCard.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+function MediumCard({ img, title }) {
+  return (
+    <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
+      <div className="relative h-80 w-80">
+        <Image src={img} className="rounded-xl" layout="fill" />
+      </div>
+
+      <div>
+        <h3>{title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default MediumCard;
